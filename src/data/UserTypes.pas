e3ca000c1b5a5dcc04ec3ecdac651810c422e9e3
ix3twastaken <harkoiiika38@gmail.com>
2026-05-17 20:16:12 +0300

    feat(sodium): add AEAD XChaCha20-Poly1305 bindings

--- src/data/UserTypes.pas
+++ src/data/UserTypes.pas
@@ -2,12 +2,18 @@ unit UserTypes;
 
 interface
 
+uses System.SysUtils;
+
+const
+  MAX_PASSWORD_SIZE = 512;
+
 type
   TUserDataRecord = packed record
     ID: integer;
     ServiceName: string[255];
     Login: string[255];
-    Password: string[255];
+    PasswordSize: UInt16;
+    Password: array[0..MAX_PASSWORD_SIZE - 1] of Byte;
     Note: string[255];
   end;
 
--- src/libs/libsodium/Sodium.pas
+++ src/libs/libsodium/Sodium.pas
@@ -5,6 +5,11 @@ interface
 uses
   System.SysUtils;
 
+const
+  crypto_aead_xchacha20poly1305_ietf_KEYBYTES = 32;
+  crypto_aead_xchacha20poly1305_ietf_NPUBBYTES = 24;
+  crypto_aead_xchacha20poly1305_ietf_ABYTES = 16;
+
 procedure randombytes_buf(buf: Pointer; size: NativeUInt); cdecl;
   external 'libsodium.dll';
 
@@ -16,6 +21,32 @@ function crypto_pwhash(outbuf: PByte; outlen: UInt64;
   salt: PByte;
   opslimit: UInt64; memlimit: NativeUInt;
   alg: Integer): Integer; cdecl;
+  external 'libsodium.dll';
+
+function crypto_aead_xchacha20poly1305_ietf_encrypt(
+  c: PByte;
+  clen_p: PUInt64;
+  m: PByte;
+  mlen: UInt64;
+  ad: PByte;
+  adlen: UInt64;
+  nsec: PByte;
+  npub: PByte;
+  k: PByte
+): Integer; cdecl;
+  external 'libsodium.dll';
+
+function crypto_aead_xchacha20poly1305_ietf_decrypt(
+  m: PByte;
+  mlen_p: PUInt64;
+  nsec: PByte;
+  c: PByte;
+  clen: UInt64;
+  ad: PByte;
+  adlen: UInt64;
+  npub: PByte;
+  k: PByte
+): Integer; cdecl;
   external 'libsodium.dll';
 
 implementation
